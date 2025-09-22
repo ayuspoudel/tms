8679510083be817fc2f729fec5f447abbc5231b0
Ayush Poudel <ayushpoudel@usf.edu>
2025-09-22 12:32:08 -0400

    Refresh

--- pulumi/user-service-pulumi/src/outputs.ts
+++
@@ -1,8 +0,0 @@
-
-import { userPool } from "./cognito";
-import { userProfileTable } from "./dynamodb";
-
-// Export important outputs
-
-export const cognitoPoolId = userPool.id;
-export const dynamoTableName = userProfileTable.name;
--- pulumi/user-service-pulumi/src/variables.ts
+++
@@ -1,7 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-
-const config = new pulumi.Config();
-
-export const awsRegion = config.require("aws:region");
-export const lambdaBucket = config.require("lambdaBucket");
-export const lambdaKey = config.require("lambdaKey");
