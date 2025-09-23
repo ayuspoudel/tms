31da2d62b5e3999cbb2c9648c571add22b14c972
Ayush Poudel <ayushpoudel@usf.edu>
2025-09-23 14:23:49 -0400

    Fix errors in db

--- user-service/src/repositories/UserSessionRepository.js
+++ user-service/src/repositories/UserSessionRepository.js
@@ -18,26 +18,26 @@ export class UserSessionRepository {
       createdAt: new Date().toISOString(),
     };
 
-    const command = new PutCommand({
-      TableName: TABLE_NAME,
-      Item: session, // capital I in v3
-    });
+    await dynamo.send(
+      new PutCommand({
+        TableName: TABLE_NAME,
+        Item: session, // must be "Item" in v3
+      })
+    );
 
-    await dynamo.send(command);
     return session;
   }
 
   async findBySession(refreshToken) {
-    const command = new QueryCommand({
-      TableName: TABLE_NAME,
-      IndexName: "refreshToken-index",
-      KeyConditionExpression: "refreshToken = :rt",
-      ExpressionAttributeValues: {
-        ":rt": refreshToken,
-      },
-    });
+    const res = await dynamo.send(
+      new QueryCommand({
+        TableName: TABLE_NAME,
+        IndexName: "refreshToken-index",
+        KeyConditionExpression: "refreshToken = :rt",
+        ExpressionAttributeValues: { ":rt": refreshToken },
+      })
+    );
 
-    const res = await dynamo.send(command);
     if (!res.Count || res.Count === 0) return null;
     return res.Items[0];
   }
@@ -46,34 +46,35 @@ export class UserSessionRepository {
     const session = await this.findBySession(refreshToken);
     if (!session) return null;
 
-    const command = new DeleteCommand({
-      TableName: TABLE_NAME,
-      Key: { id: session.id },
-    });
+    await dynamo.send(
+      new DeleteCommand({
+        TableName: TABLE_NAME,
+        Key: { id: session.id },
+      })
+    );
 
-    await dynamo.send(command);
     return session;
   }
 
   async deleteByUser(userId) {
-    const queryCommand = new QueryCommand({
-      TableName: TABLE_NAME,
-      IndexName: "userId-index",
-      KeyConditionExpression: "userId = :uid",
-      ExpressionAttributeValues: {
-        ":uid": userId,
-      },
-    });
+    const res = await dynamo.send(
+      new QueryCommand({
+        TableName: TABLE_NAME,
+        IndexName: "userId-index",
+        KeyConditionExpression: "userId = :uid",
+        ExpressionAttributeValues: { ":uid": userId },
+      })
+    );
 
-    const res = await dynamo.send(queryCommand);
     if (!res.Count || res.Count === 0) return;
 
     for (const session of res.Items) {
-      const deleteCommand = new DeleteCommand({
-        TableName: TABLE_NAME,
-        Key: { id: session.id },
-      });
-      await dynamo.send(deleteCommand);
+      await dynamo.send(
+        new DeleteCommand({
+          TableName: TABLE_NAME,
+          Key: { id: session.id },
+        })
+      );
     }
   }
 }
